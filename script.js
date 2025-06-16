29ba593b60d60320ffadf34283495f986d9097c9
NICKPOLUKITAS <162640270+NICKPOLUKITAS@users.noreply.github.com>
2025-06-16 20:16:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.getElementById('botao').addEventListener('click', function() {
-    const time1 = document.getElementById('time1').value || "Time 1";
-    const time2 = document.getElementById('time2').value || "Time 2";
+function simular() {
+    const time1 = document.getElementById('time1').value.trim() || "Time 1";
+    const time2 = document.getElementById('time2').value.trim() || "Time 2";
 
     const golsTime1 = Math.floor(Math.random() * 5); // de 0 a 4
     const golsTime2 = Math.floor(Math.random() * 5);
@@ -8,4 +8,4 @@ document.getElementById('botao').addEventListener('click', function() {
     const resultado = ${time1} ${golsTime1} x ${golsTime2} ${time2};
 
     document.getElementById('resultado').innerText = resultado;
-});
+}
