718dec10a22717c97ed5bb1f9ddbb84cf20c2f1b
NICKPOLUKITAS <162640270+NICKPOLUKITAS@users.noreply.github.com>
2025-06-16 19:48:32 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function simular() {
+    const time1 = document.getElementById('time1').value || "Time 1";
+    const time2 = document.getElementById('time2').value || "Time 2";
+
+    const golsTime1 = Math.floor(Math.random() * 5); // de 0 a 4
+    const golsTime2 = Math.floor(Math.random() * 5);
+
+    const resultado = ${time1} ${golsTime1} x ${golsTime2} ${time2};
+
+    document.getElementById('resultado').innerText = resultado;
+}
